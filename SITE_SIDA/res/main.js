2ddf61e6e4913ee2fa3c392955616cc1de31041b
Poke <blespinasse.pro@gmail.com>
2022-12-01 20:58:28 +0100

    Co-authored-by: camitons <camitons@users.noreply.github.com> Co-authored-by: Aurel <Unmoriel@users.noreply.github.com>

--- SITE_SIDA/res/main.js
+++ SITE_SIDA/res/main.js
@@ -1,9 +1,11 @@
 
 
+
+
 //une fonction permettant de changer quelque chose toute les 30 secondes
 var defaultTimeSeconds = 30;
 var secondLeft = defaultTimeSeconds;
-
+var boolean
 
 
 setInterval(function() {
@@ -31,16 +33,24 @@ elt4.addEventListener('click', choice(4) {
 
 })
 
+//ajout element appuyer sur entrée ssi déjà choix question
+const nextquest = document.getElementById('jaaj');
+nextquest.addEventListener('keypress', function(e) {
+
+})
 //document.getElementById("myDIV").style.display = "none"; 
 
 //setInterval(function, milliseconds)
 //---------------------FONCTIONS-----------------------
 const buttons = [elt1, elt2, elt3, elt4];
 
+//est appelée une fois que l'information a été affichée et que le client ai cliqué sur entrée
+//permet de changer l'image de background et l'intérieur des bouttons
 function passNextQuestion() {
     
 }
-
+//est appelée lors du clique du choix
+//permet d'enlever l'affichage des autres bouttons
 function choice(a){
     for (let i; i<buttons.length;i++){
         if (a != i){
@@ -50,12 +60,12 @@ function choice(a){
     textereponse.style.display="block";
 }
 
-function cleanboard()
-
+function cleanboard(){
     for (let i; i<buttons.length;i++){
         buttons[i].style.display="none";
     }
-    
+
+}
 
     
 
