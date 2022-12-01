94d45e299a7e4e728e7fd37717433da2aa98ff2f
Poke <blespinasse.pro@gmail.com>
2022-12-01 21:29:38 +0100

    up

--- SITE_SIDA/backend/connect_db.php
+++ SITE_SIDA/backend/connect_db.php
@@ -7,9 +7,5 @@ $db = "Iut_Ndi_SexInfo";
 // Create connection
 $conn = new mysqli($servername, $username, $password, $db);
 
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-echo "Connected successfully";
+
 ?>
