f09c50651031139d322de2cff6b340a58382e68b
DorianLeGeek <DodoLeDev_NDI@github.com>
2022-12-01 19:37:26 +0100

    Responsive ++

---
+++ QuizzDB.js
@@ -0,0 +1,9 @@
+const quiz = [
+    {
+        question: "Tu entends une personne être lourde (obscène...) avec une autre. Que fais-tu ?",
+        optionA: "Ne rien faire",
+        optionB: "Distraire l'élément perturbateur",
+        optionC: "Filmer la scène",
+        optionD: "7 days"
+    }
+]
